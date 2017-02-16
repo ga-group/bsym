8f37ee8dfb1d75cbe894dd00081110d89139a2ec
Sebastian Freundt <freundt@ga-group.nl>
2017-02-16 05:50:08 +0000

    chore, put raw stuff into archive

--- scripts/ttlify-boff.sh
+++ scripts/ttlify-boff.sh
@@ -7,6 +7,6 @@ for i in "${WD}/download-nobackup"/*dif*; do
 	bsdcat "${i}"
 	rm -f -- "${i}"
 done \
-	| tee >(xz -c > "${WD}/tmp/raw_${NOW}.xz") \
+	| tee >(xz -c > "${WD}/archive/raw_${NOW}.xz") \
 	| "${WD}/scripts/ttlify-boff.awk" --diff \
 	| tee >(gzip -c > "${WD}/tmp/ttl.d_${NOW}.gz")
